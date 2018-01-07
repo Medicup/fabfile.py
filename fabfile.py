0d1638dc19f8b791fc7bd93380554e4ec3505fe1
Stephen <medicup@gmail.com>
2018-01-06 21:38:33 -0500

    Update fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python
 from __future__ import print_function
-from fabric.api import env, sudo, run
+from fabric.api import env, sudo, run , roles
 import os.path
 
+env.combine_stderr = True
 env.output_prefix = False
 env.user = 'stitched'
-env.hosts = [ '172.16.210.81', '172.16.210.82' 
-  '172.16.210.83', '172.16.210.84', '172.16.210.85']
+env.roledefs = {
+  'local': ['localhost'],
+  'wb': [ 'wb201.bluerage.lan', 'wb202.bluerage.lan' ],
+  'ns': [ 'ns01.bluerage.lan'],
+  'db': [ 'db211.bluerage.lan', 'db212.bluerage.lan', 'db213.bluerage.lan']
+  'testwb': ['172.16.210.81', '172.16.210.82']
+  'testdb'; ['172.16.210.83', '172.16.210.84', '172.16.210.85']
+
+}
 
 def deploy():
-  programs = ['vim', 'dnsutils', 'ccze', 'iftop',
-    'htop', 'curl', 'openssh-server', 'openssh-client', 
-    'iptables-persistent', 'python-pip', 'git']
+  programs = ['vim', 'dnsutils', 'ccze', 'iftop', 'htop', 'curl', 'openssh-server', 'openssh-client', 'iptables-persistent', 'python-pip', 'git']
 
   for program in programs:
     sudo('apt-get install -y %s' % (program))
@@ -21,6 +27,7 @@ def update():
   updates = ['update', 'upgrade', 'remove', 'autoclean']
 
   for update in updates:
+    print('Initiating %s function... ' % (update))
     sudo('apt -y %s' % (update))
 
   if os.path.isfile('/var/run/reboot-required') == True:
